cfda5a45bc991c86e105f108a26dfa8a0ed411e2
yelnats10 <yelnats10@gmail.com>
2017-10-07 10:15:26 -0400

    auth gone

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -10,29 +10,6 @@ $(document).ready(function() {
     };
     firebase.initializeApp(config);
 
-    var provider = new firebase.auth.GithubAuthProvider();
-    // console.log(provider);
-    firebase.auth().signInWithRedirect(provider);
-
-    firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
     var database = firebase.database();
 
     var trainName = "";
